8ca663535d3aa666dbe28771dcb87f16bf6f6ede
hinto.janai <hinto.janai@protonmail.com>
2023-12-07 20:41:38 -0500

    match `main.rs` to `cli.rs`

--- cuprate/src/main.rs
+++ cuprate/src/main.rs
@@ -5,24 +5,22 @@ mod docs;
 mod macros;
 
 fn main() {
-	// Handle CLI arguments.
-	let (dry_run, log, config_cmd) = {
-		// Nothing was passed, only `./cuprate`,
-		// use the default values.
-		if std::env::args_os().len() == 1 {
-			(false, None, None)
-		} else {
-			// Else, run all the `clap` code (cli.rs).
-			crate::cli::Cli::get()
-		}
-	};
-
 	// Set `umask` for the entire process.
 	// Files are `rw-r-x---`, folders are `rwx-r-x---`
 	// https://docs.rs/disk/latest/disk/fn.umask.html
 	//
 	// TODO: set a reasonable value. Also, this does nothing on Windows.
 	disk::umask(0o027);
+
+	// Handle CLI arguments.
+	let cli = if std::env::args_os().len() > 1 {
+		// Some arguments were passed, run all the `clap` code (cli.rs).
+		crate::cli::Cli::init()
+	} else {
+		// Nothing was passed, only `./cuprate`,
+		// use the default values.
+		crate::cli::CliResult::DEFAULT
+	};
 
 	#[allow(non_snake_case)] // This is a reference to a `static` defined in `crate::config`.
 	// Merge CLI options with on-disk config and init the logger.
@@ -34,10 +32,10 @@ fn main() {
 	// 1. We want to log within `init()`, but...
 	// 2. We can't be sure what the true `--log-level` is
 	//    until both CLI + disk Config are merged
-	let CONFIG: &'static crate::config::Config = crate::config::ConfigBuilder::init(log, config_cmd);
+	let CONFIG: &'static crate::config::Config = crate::config::ConfigBuilder::init(cli.log_level, cli.config);
 
 	// If `dry_run`, print config/stats/etc and exit cleanly.
-	if dry_run {
+	if cli.dry_run {
 		println!("{}", serde_json::to_string_pretty(CONFIG).unwrap());
 		std::process::exit(0);
 	}
@@ -46,7 +44,7 @@ fn main() {
 	todo!("cuprate node .. cleaning cache");
 
 	// Start `cuprate` (node init, other thread, TBD).
-	let result = todo!("cuprate node ... init"); // <----------------- Program hangs here as the "node"
+	let result: Result<(), anyhow::Error> = todo!("cuprate node ... init"); // <----------------- Program hangs here as the "node"
 
 	// Graceful shutdown.
 	// - Cleanup cache
