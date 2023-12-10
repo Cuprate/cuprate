f5f7d956d05362a6f8a2139b2f8f5afc58d1de33
hinto.janai <hinto.janai@protonmail.com>
2023-12-10 15:31:38 -0500

    fix tests and fmt

--- json-rpc/src/error/code.rs
+++ json-rpc/src/error/code.rs
@@ -4,38 +4,37 @@ use serde::{Deserialize, Deserializer, Serialize, Serializer};
 //---------------------------------------------------------------------------------------------------- Constants
 #[rustfmt::skip]
 mod constants {
-    // [`JSON-RPC 2.0`](https://www.jsonrpc.org/specification#error_object) defined errors.
-	pub const PARSE_ERROR:      (i32, &str) = (-32700, "Parse error");
-	pub const INVALID_REQUEST:  (i32, &str) = (-32600, "Invalid Request");
-	pub const METHOD_NOT_FOUND: (i32, &str) = (-32601, "Method not found");
-	pub const INVALID_PARAMS:   (i32, &str) = (-32602, "Invalid params");
-	pub const INTERNAL_ERROR:   (i32, &str) = (-32603, "Internal error");
+   // [`JSON-RPC 2.0`](https://www.jsonrpc.org/specification#error_object) defined errors.
+   pub const PARSE_ERROR:      (i32, &str) = (-32700, "Parse error");
+   pub const INVALID_REQUEST:  (i32, &str) = (-32600, "Invalid Request");
+   pub const METHOD_NOT_FOUND: (i32, &str) = (-32601, "Method not found");
+   pub const INVALID_PARAMS:   (i32, &str) = (-32602, "Invalid params");
+   pub const INTERNAL_ERROR:   (i32, &str) = (-32603, "Internal error");
+   // Server-defined error.
+   //
+   // The [`i32`] error code is the caller's choice.
+   pub const SERVER_ERROR: &str = "Server error";
 
-    // These exist because when `match`'ing, you cannot
-    // do `TUPLE_CONST.0`, see [`ErrorCode::from_code`] below.
-    pub(super) const PARSE_ERROR_I32:      i32 = PARSE_ERROR.0;
-	pub(super) const INVALID_REQUEST_I32:  i32 = INVALID_REQUEST.0;
-	pub(super) const METHOD_NOT_FOUND_I32: i32 = METHOD_NOT_FOUND.0;
-	pub(super) const INVALID_PARAMS_I32:   i32 = INVALID_PARAMS.0;
-	pub(super) const INTERNAL_ERROR_I32:   i32 = INTERNAL_ERROR.0;
+   // These exist because when `match`'ing, you cannot
+   // do `TUPLE_CONST.0`, see [`ErrorCode::from_code`] below.
+   pub(super) const PARSE_ERROR_I32:      i32 = PARSE_ERROR.0;
+   pub(super) const INVALID_REQUEST_I32:  i32 = INVALID_REQUEST.0;
+   pub(super) const METHOD_NOT_FOUND_I32: i32 = METHOD_NOT_FOUND.0;
+   pub(super) const INVALID_PARAMS_I32:   i32 = INVALID_PARAMS.0;
+   pub(super) const INTERNAL_ERROR_I32:   i32 = INTERNAL_ERROR.0;
 
-	// Server-defined error.
-    //
-    // The [`i32`] error code is the caller's choice.
-	pub const SERVER_ERROR: &str = "Server error";
-
-	// Common custom errors.
-	pub const UNKNOWN_ERROR:            (i32, &str) = (-32000, "Unknown error");
-	pub const BATCH_NOT_SUPPORTED:      (i32, &str) = (-32001, "Batched requests are not supported by this server");
-	pub const LIMIT_REQUEST:            (i32, &str) = (-32002, "Request limit exceeded");
-	pub const LIMIT_RESPONSE:           (i32, &str) = (-32003, "Response limit exceeded");
-	pub const LIMIT_BATCH_REQUEST:      (i32, &str) = (-32004, "Batch request limit exceeded");
-	pub const LIMIT_BATCH_RESPONSE:     (i32, &str) = (-32005, "Batch response limit exceeded");
-	pub const OVERSIZED_REQUEST:        (i32, &str) = (-32006, "Request is too big");
-	pub const OVERSIZED_RESPONSE:       (i32, &str) = (-32007, "Response is too big");
-	pub const OVERSIZED_BATCH_REQUEST:  (i32, &str) = (-32008, "The batch request was too large");
-	pub const OVERSIZED_BATCH_RESPONSE: (i32, &str) = (-32009, "The batch request was too large");
-	pub const SERVER_IS_BUSY:           (i32, &str) = (-32010, "Server is busy");
+   // Common custom errors.
+   pub const UNKNOWN_ERROR:            (i32, &str) = (-32000, "Unknown error");
+   pub const BATCH_NOT_SUPPORTED:      (i32, &str) = (-32001, "Batched requests are not supported by this server");
+   pub const LIMIT_REQUEST:            (i32, &str) = (-32002, "Request limit exceeded");
+   pub const LIMIT_RESPONSE:           (i32, &str) = (-32003, "Response limit exceeded");
+   pub const LIMIT_BATCH_REQUEST:      (i32, &str) = (-32004, "Batch request limit exceeded");
+   pub const LIMIT_BATCH_RESPONSE:     (i32, &str) = (-32005, "Batch response limit exceeded");
+   pub const OVERSIZED_REQUEST:        (i32, &str) = (-32006, "Request is too big");
+   pub const OVERSIZED_RESPONSE:       (i32, &str) = (-32007, "Response is too big");
+   pub const OVERSIZED_BATCH_REQUEST:  (i32, &str) = (-32008, "The batch request was too large");
+   pub const OVERSIZED_BATCH_RESPONSE: (i32, &str) = (-32009, "The batch request was too large");
+   pub const SERVER_IS_BUSY:           (i32, &str) = (-32010, "Server is busy");
 }
 pub use constants::*;
 
@@ -43,24 +42,24 @@ pub use constants::*;
 /// [5.1 Error object code](https://www.jsonrpc.org/specification)
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, thiserror::Error)]
 pub enum ErrorCode {
-    #[error("{}: {}", PARSE_ERROR.0, PARSE_ERROR.1)]
+    #[error("{}", PARSE_ERROR.1)]
     /// Invalid JSON was received by the server.
     ///
     /// An error occurred on the server while parsing the JSON text.
     ParseError,
-    #[error("{}: {}", INVALID_REQUEST.0, INVALID_REQUEST.1)]
+    #[error("{}", INVALID_REQUEST.1)]
     /// The JSON sent is not a valid Request object.
     InvalidRequest,
-    #[error("{}: {}", METHOD_NOT_FOUND.0, METHOD_NOT_FOUND.1)]
+    #[error("{}", METHOD_NOT_FOUND.1)]
     /// The method does not exist / is not available.
     MethodNotFound,
-    #[error("{}: {}", INVALID_PARAMS.0, INVALID_PARAMS.1)]
+    #[error("{}", INVALID_PARAMS.1)]
     /// Invalid method parameters.
     InvalidParams,
-    #[error("{}: {}", INTERNAL_ERROR.0, INTERNAL_ERROR.1)]
+    #[error("{}", INTERNAL_ERROR.1)]
     /// Internal JSON-RPC error.
     InternalError,
-    #[error("{SERVER_ERROR}: {0}")]
+    #[error("{SERVER_ERROR} {0}")]
     /// Reserved for implementation-defined server-errors.
     ServerError(i32),
 }
@@ -169,6 +168,6 @@ mod tests {
 
         let err = ErrorCode::ServerError(code);
         assert_eq!(err.code(), code);
-        assert_eq!(format!("Server error: {code}"), err.to_string());
+        assert_eq!(format!("Server error {code}"), err.to_string());
     }
 }
