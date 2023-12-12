2318797fda0ba7b893237e0de1bc88b2ca86f949
hinto.janai <hinto.janai@protonmail.com>
2023-12-12 11:51:00 -0500

    add `crypto.rs`

---
+++ helper/src/crypto.rs
@@ -0,0 +1,49 @@
+//! Crypto related
+
+//---------------------------------------------------------------------------------------------------- Use
+use curve25519_dalek::edwards::CompressedEdwardsY;
+
+//---------------------------------------------------------------------------------------------------- Public API
+#[inline]
+/// Checks that a point is canonical.
+///
+/// TODO: non-canonical points are ignored, and will return true?
+///
+/// ```rust
+/// # use helper::crypto::*;
+/// # use curve25519_dalek::edwards::CompressedEdwardsY;
+/// let slice = [1; 32];
+/// let point = CompressedEdwardsY(slice);
+/// assert!(check_point(&point));
+///
+/// // non_canonical_bytes_because_unreduced
+/// let slice = [16; 32];
+/// let point = CompressedEdwardsY(slice);
+/// assert!(check_point(&point));
+///
+/// // non_canonical_bytes_because_highbit
+/// let slice = [
+///   0, 0, 0, 0, 0, 0, 0, 0,
+///   0, 0, 0, 0, 0, 0, 0, 0,
+///   0, 0, 0, 0, 0, 0, 0, 0,
+///   0, 0, 0, 0, 0, 0, 0, 128,
+/// ];
+/// let point = CompressedEdwardsY(slice);
+/// assert!(check_point(&point));
+/// ```
+///
+/// https://github.com/dalek-cryptography/curve25519-dalek/issues/380
+pub fn check_point(point: &CompressedEdwardsY) -> bool {
+    let bytes = point.as_bytes();
+
+    point
+        .decompress()
+        // Ban points which are either unreduced or -0
+        .filter(|point| point.compress().as_bytes() == bytes)
+        .is_some()
+}
+
+//---------------------------------------------------------------------------------------------------- Tests
+#[cfg(test)]
+mod test {
+}
