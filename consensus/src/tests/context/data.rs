78244a9d161cc1581d2f61c2b37da53ed05ccbd2
Boog900 <54e72d8a-345f-4599-bd90-c6b9bc7d0ec5@aleeas.com>
2024-01-19 23:52:03 +0000

    use static for big const arrays

--- consensus/src/tests/context/data.rs
+++ consensus/src/tests/context/data.rs
@@ -1,8 +1,8 @@
 use monero_consensus::HardFork;
 
-pub const HFS_2688888_2689608: [(HardFork, HardFork); 720] = include!("./data/hfs_2688888_2689608");
+pub static HFS_2688888_2689608: [(HardFork, HardFork); 720] = include!("./data/hfs_2688888_2689608");
 
-pub const HFS_2678808_2688888: [(HardFork, HardFork); 10080] =
+pub static HFS_2678808_2688888: [(HardFork, HardFork); 10080] =
     include!("./data/hfs_2678808_2688888");
 
-pub const BW_2850000_3050000: [(usize, usize); 200_000] = include!("./data/bw_2850000_3050000");
+pub static BW_2850000_3050000: [(usize, usize); 200_000] = include!("./data/bw_2850000_3050000");
