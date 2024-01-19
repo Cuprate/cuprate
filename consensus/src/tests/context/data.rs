d10b9d3f8b87503c01b07ddb718cae9b518265ef
Boog900 <54e72d8a-345f-4599-bd90-c6b9bc7d0ec5@aleeas.com>
2024-01-19 23:56:06 +0000

    fmt

--- consensus/src/tests/context/data.rs
+++ consensus/src/tests/context/data.rs
@@ -1,6 +1,7 @@
 use monero_consensus::HardFork;
 
-pub static HFS_2688888_2689608: [(HardFork, HardFork); 720] = include!("./data/hfs_2688888_2689608");
+pub static HFS_2688888_2689608: [(HardFork, HardFork); 720] =
+    include!("./data/hfs_2688888_2689608");
 
 pub static HFS_2678808_2688888: [(HardFork, HardFork); 10080] =
     include!("./data/hfs_2678808_2688888");
