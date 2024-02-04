ee6341bf6a440213bad7c00259c49c7380f62689
hinto.janai <hinto.janai@protonmail.com>
2024-02-04 15:54:38 -0500

    base `database.rs`

--- database/src/database.rs
+++ database/src/database.rs
@@ -1,10 +1,47 @@
 //! TODO
 
 //---------------------------------------------------------------------------------------------------- Import
+use crate::error::Error;
+
+use std::path::Path;
 
 //---------------------------------------------------------------------------------------------------- Constants
 
 //---------------------------------------------------------------------------------------------------- TYPE
+/// TODO
+///
+/// Database trait abstraction.
+pub trait Database<'env>: Sized {
+    //------------------------------------------------ Types
+    /// Read-only transaction.
+    type RoTx: 'env;
+
+    /// Read/write transaction.
+    type RwTx: 'env;
+
+    //------------------------------------------------ Required
+    /// TODO
+    /// # Errors
+    /// TODO
+    fn open<P: AsRef<Path>>(path: P) -> Result<Self, Error>;
+
+    /// TODO
+    /// # Errors
+    /// TODO
+    fn sync(&self) -> Result<(), Error>;
+
+    /// TODO
+    /// # Errors
+    /// TODO
+    fn tx_ro(&self) -> Result<Self::RoTx, Error>;
+
+    /// TODO
+    /// # Errors
+    /// TODO
+    fn tx_rw(&self) -> Result<Self::RwTx, Error>;
+
+    //------------------------------------------------ Provided
+}
 
 //---------------------------------------------------------------------------------------------------- IMPL
 
