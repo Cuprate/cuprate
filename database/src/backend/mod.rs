1e3ce037a8e80632af645845665d53ce57affe83
hinto.janai <hinto.janai@protonmail.com>
2024-02-04 15:53:51 -0500

    add `backend/` structure

--- database/src/backend/mod.rs
+++ database/src/backend/mod.rs
@@ -1,3 +1,11 @@
 //! TODO
 
-mod heed;
+cfg_if::cfg_if! {
+    if #[cfg(feature = "sanakirja")] {
+        mod sanakirja;
+        pub use sanakirja::Sanakirja as ConcreteDatabase;
+    } else {
+        mod heed;
+        pub use heed::Heed as ConcreteDatabase;
+    }
+}
