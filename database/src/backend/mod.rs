eabc19a5752813fa2095493e415abac9088c9ea0
hinto.janai <hinto.janai@protonmail.com>
2024-02-04 16:10:43 -0500

    backend: add `DATABASE_BACKEND`

--- database/src/backend/mod.rs
+++ database/src/backend/mod.rs
@@ -4,8 +4,12 @@ cfg_if::cfg_if! {
     if #[cfg(feature = "sanakirja")] {
         mod sanakirja;
         pub use sanakirja::Sanakirja as ConcreteDatabase;
+        /// Static string of the `crate` being used as the database backend.
+        pub const DATABASE_BACKEND: &str = "sanakirja";
     } else {
         mod heed;
         pub use heed::Heed as ConcreteDatabase;
+        /// Static string of the `crate` being used as the database backend.
+        pub const DATABASE_BACKEND: &str = "heed";
     }
 }
