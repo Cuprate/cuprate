4ce1bd463643bb8e8332a353f534b23a0f8cd8fe
hinto.janai <hinto.janai@protonmail.com>
2024-02-04 16:11:02 -0500

    base `error.rs`

--- database/src/error.rs
+++ database/src/error.rs
@@ -1,10 +1,66 @@
 //! TODO
 
 //---------------------------------------------------------------------------------------------------- Import
+use crate::backend::DATABASE_BACKEND;
+
+use std::borrow::Cow;
+use std::fmt::Debug;
 
 //---------------------------------------------------------------------------------------------------- Constants
 
-//---------------------------------------------------------------------------------------------------- TYPE
+//---------------------------------------------------------------------------------------------------- InitError
+/// TODO
+///
+/// Database errors that occur during initialization.
+///
+/// `BackendError` is an error specifically from the
+/// database backend being used. TODO: this may not
+/// be needed if we can convert all error types into
+/// "generic" database errors.
+#[derive(thiserror::Error, Debug)]
+pub enum InitError<BackendError: Debug> {
+    /// TODO
+    #[error("database PATH is inaccessible: {0}")]
+    Path(std::io::Error),
+
+    /// TODO
+    #[error("{DATABASE_BACKEND} error: {0}")]
+    Backend(BackendError),
+
+    /// TODO
+    ///
+    /// An unknown error occured.
+    #[error("unknown error: {0}")]
+    Unknown(Cow<'static, str>),
+}
+
+//---------------------------------------------------------------------------------------------------- RuntimeError
+/// TODO: `InitError/RuntimeError` are maybe bad names.
+///
+/// Database errors that occur _after_ successful initialization.
+#[derive(thiserror::Error, Debug)]
+pub enum RuntimeError {
+    // TODO: borsh::io::Error?
+    ///
+    /// An error occured when attempting to
+    /// serialize the key data into bytes.
+    #[error("serialize error: {0}")]
+    Serialize(borsh::io::Error),
+
+    // TODO: borsh::io::Error?
+    ///
+    /// An error occured when attempting to
+    /// deserialize the response value from
+    /// the database.
+    #[error("deserialize error: {0}")]
+    Deserialize(borsh::io::Error),
+
+    /// TODO
+    ///
+    /// An unknown error occured.
+    #[error("unknown error: {0}")]
+    Unknown(Cow<'static, str>),
+}
 
 //---------------------------------------------------------------------------------------------------- IMPL
 
