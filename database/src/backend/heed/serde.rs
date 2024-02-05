9385aa94e1b2386c9b10d86f01fac8f9f46f0171
hinto.janai <hinto.janai@protonmail.com>
2024-02-05 17:35:18 -0500

    heed: impl `BytesEncode`, `BytesDecode`

--- database/src/backend/heed/serde.rs
+++ database/src/backend/heed/serde.rs
@@ -11,24 +11,43 @@ use std::borrow::Cow;
 /// Implement `heed` (de)serialization traits
 /// for anything that implements [`crate::pod::Pod`].
 ///
-/// Blanket implementation runs into some errors, so this is used instead.
+/// Blanket implementation breaks orphan impl rules, so this is used instead.
 macro_rules! impl_heed {
     ($(
-        $t:ty // The type that implements [`crate::pod::Pod`], to implement heed serde traits.
+        $name:ident => // The name that implements [`crate::pod::Pod`]
+        $t:ident       // The type to (de)serialize into/from
     ),* $(,)?) => {
         $(
-            impl<'a> heed::BytesEncode<'a> for $t {
+            // `heed` Encode.
+            impl<'a> heed::BytesEncode<'a> for $name {
                 type EItem = $t;
 
-                fn bytes_encode(item: &'a $t) -> Result<Cow<'a, [u8]>, heed::BoxedError> {
-                    Ok(Cow::Borrowed(Pod::as_bytes(item).as_ref()))
+                fn bytes_encode(item: &'a Self::EItem) -> Result<Cow<'a, [u8]>, heed::BoxedError> {
+                    Ok(item.into_bytes())
+                }
+            }
+
+            // `heed` Decode.
+            impl<'a> heed::BytesDecode<'a> for $name {
+                type DItem = $t;
+
+                fn bytes_decode(bytes: &'a [u8]) -> Result<Self::DItem, heed::BoxedError> {
+                    match Pod::from_bytes(bytes) {
+                        Ok(o) => Ok(o),
+                        Err(e) => todo!(),
+                    }
                 }
             }
         )*
     };
 }
 
-impl_heed! {}
+/// TODO
+struct Test;
+
+impl_heed! {
+    Test => u8,
+}
 
 //---------------------------------------------------------------------------------------------------- Tests
 #[cfg(test)]
