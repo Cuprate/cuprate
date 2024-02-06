e1ed40f6f3328a98215e81d2df7a7ced3b329d7f
hinto.janai <hinto.janai@protonmail.com>
2024-02-05 21:09:42 -0500

    service: add `service.rs`

--- database/src/service/mod.rs
+++ database/src/service/mod.rs
@@ -1,1 +1,10 @@
 //! TODO
+
+mod readers;
+pub(crate) use readers::Readers;
+
+mod writer;
+pub(crate) use writer::Writer;
+
+mod service;
+pub use service::DatabaseService;
---
+++ database/src/service/service.rs
@@ -0,0 +1,70 @@
+//! TODO
+
+//---------------------------------------------------------------------------------------------------- Import
+use crate::{
+    service::{Readers, Writer},
+    ConcreteDatabase,
+};
+
+use std::task::{Context, Poll};
+
+//---------------------------------------------------------------------------------------------------- Constants
+
+//---------------------------------------------------------------------------------------------------- DatabaseService
+/// TODO: maybe `CuprateDatabase`?
+///
+/// Manager
+#[allow(dead_code)] // TODO
+pub struct DatabaseService {
+    /// TODO
+    db: ConcreteDatabase, // TODO: either `Arc` or `&'static` after `Box::leak`
+
+    /// TODO
+    readers: Readers,
+
+    /// TODO
+    writer: Writer,
+}
+
+//---------------------------------------------------------------------------------------------------- DatabaseService Impl
+impl DatabaseService {
+    /// TODO
+    pub fn new(db: ConcreteDatabase) -> Self {
+        #[allow(unused_variables)]
+        let this = Self {
+            db,
+            readers: Readers,
+            writer: Writer,
+        };
+
+        todo!()
+    }
+}
+
+//---------------------------------------------------------------------------------------------------- Trait Impl
+/// TODO - temporary struct for gist.
+pub struct Request;
+/// TODO - temporary struct for gist.
+pub struct Response;
+/// TODO - temporary struct for gist.
+pub struct Error;
+
+impl tower::Service<Request> for DatabaseService {
+    type Response = Response;
+    type Error = Error;
+    type Future = std::future::Ready<Result<Response, Error>>;
+
+    fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        todo!()
+    }
+
+    fn call(&mut self, _req: Request) -> Self::Future {
+        todo!()
+    }
+}
+
+//---------------------------------------------------------------------------------------------------- Tests
+#[cfg(test)]
+mod test {
+    // use super::*;
+}
