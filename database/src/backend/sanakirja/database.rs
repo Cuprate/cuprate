2a4843e5ea9c9fda8f177ccf45b2fdc30212deb1
hinto.janai <hinto.janai@protonmail.com>
2024-02-08 20:53:10 -0500

    sanakirja: impl `Database`

---
+++ database/src/backend/sanakirja/database.rs
@@ -0,0 +1,40 @@
+//! TODO
+
+//---------------------------------------------------------------------------------------------------- Import
+use crate::{database::Database, error::RuntimeError, table::Table};
+
+//---------------------------------------------------------------------------------------------------- Constants
+
+//---------------------------------------------------------------------------------------------------- Database Impls
+impl<T: Table> Database<T> for sanakirja::btree::Db<T::Key, T::Value> {
+    fn get(&self, key: &T::Key) -> Result<Option<T::Value>, RuntimeError> {
+        todo!()
+    }
+
+    #[allow(refining_impl_trait)] // TODO: add back `impl Iterator`
+    fn get_range(
+        &self,
+        key: &T::Key,
+        amount: usize,
+    ) -> Result<std::vec::Drain<'_, T::Value>, RuntimeError> {
+        todo!()
+    }
+
+    fn put(&mut self, key: &T::Key, value: &T::Value) -> Result<(), RuntimeError> {
+        todo!()
+    }
+
+    fn clear(&mut self) -> Result<(), RuntimeError> {
+        todo!()
+    }
+
+    fn delete(&mut self, key: &T::Key) -> Result<bool, RuntimeError> {
+        todo!()
+    }
+}
+
+//---------------------------------------------------------------------------------------------------- Tests
+#[cfg(test)]
+mod test {
+    // use super::*;
+}
