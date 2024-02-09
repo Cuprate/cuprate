7054c4efcbd53915f541dc0b86137ed631e0c914
hinto.janai <hinto.janai@protonmail.com>
2024-02-08 20:52:51 -0500

    sanakirja: impl `Env`

--- database/src/backend/sanakirja/env.rs
+++ database/src/backend/sanakirja/env.rs
@@ -1,20 +1,83 @@
 //! TODO
 
 //---------------------------------------------------------------------------------------------------- Import
-// use crate::database::Database;
+use crate::{database::Database, env::Env, error::RuntimeError, table::Table};
 
-//---------------------------------------------------------------------------------------------------- Constants
+use std::path::Path;
 
-//---------------------------------------------------------------------------------------------------- TYPE
-/// TODO
-pub struct ConcreteEnv;
+//---------------------------------------------------------------------------------------------------- ConcreteEnv
+/// A strongly typed, concrete database environment, backed by `sanakirja`.
+pub struct ConcreteEnv(sanakirja::Env);
 
-//---------------------------------------------------------------------------------------------------- IMPL
+//---------------------------------------------------------------------------------------------------- Env Impl
+impl Env for ConcreteEnv {
+    /// TODO
+    ///
+    /// We could also implement `Borrow<sanakirja::Env> for ConcreteEnv`
+    /// instead of this reference.
+    type RoTx<'db> = sanakirja::Txn<&'db sanakirja::Env>;
 
-//---------------------------------------------------------------------------------------------------- Trait Impl
-// impl Database for Sanakirja {
-// TODO
-// }
+    /// TODO
+    type RwTx<'db> = sanakirja::MutTxn<&'db sanakirja::Env, ()>;
+
+    //------------------------------------------------ Required
+    #[cold]
+    #[inline(never)] // called once.
+    /// TODO
+    /// # Errors
+    /// TODO
+    fn open<P: AsRef<Path>>(path: P) -> Result<Self, RuntimeError> {
+        todo!()
+    }
+
+    /// TODO
+    /// # Errors
+    /// TODO
+    fn sync(&self) -> Result<(), RuntimeError> {
+        todo!()
+    }
+
+    #[inline]
+    /// TODO
+    /// # Errors
+    /// TODO
+    fn ro_tx(&self) -> Result<Self::RoTx<'_>, RuntimeError> {
+        todo!()
+    }
+
+    #[inline]
+    /// TODO
+    /// # Errors
+    /// TODO
+    fn rw_tx(&self) -> Result<Self::RwTx<'_>, RuntimeError> {
+        todo!()
+    }
+
+    #[cold]
+    #[inline(never)] // called infrequently?.
+    /// TODO
+    /// # Errors
+    /// TODO
+    fn create_database<T: Table>(
+        &self,
+        tx_rw: &mut Self::RwTx<'_>,
+    ) -> Result<impl Database<T>, RuntimeError> {
+        let tx: sanakirja::btree::Db<T::Key, T::Value> = todo!();
+        Ok(tx)
+    }
+
+    #[inline]
+    /// TODO
+    /// # Errors
+    /// TODO
+    fn open_database<T: Table>(
+        &self,
+        to_rw: &Self::RoTx<'_>,
+    ) -> Result<Option<impl Database<T>>, RuntimeError> {
+        let tx: sanakirja::btree::Db<T::Key, T::Value> = todo!();
+        Ok(Some(tx))
+    }
+}
 
 //---------------------------------------------------------------------------------------------------- Tests
 #[cfg(test)]
