afaccffc8e8d957e27caaee53f39ed9663f5f1f9
hinto.janai <hinto.janai@protonmail.com>
2024-02-12 14:56:55 -0500

    backend: add missing `RoTx/RwTx` inputs

--- database/src/backend/sanakirja/database.rs
+++ database/src/backend/sanakirja/database.rs
@@ -5,12 +5,16 @@ use crate::{database::Database, error::RuntimeError, table::Table};
 
 //---------------------------------------------------------------------------------------------------- Database Impls
 impl<T: Table> Database<T> for sanakirja::btree::Db<T::Key, T::Value> {
-    fn get(&self, key: &T::Key) -> Result<Option<T::Value>, RuntimeError> {
+    type RoTx<'db> = sanakirja::Txn<&'_ sanakirja::Env>;
+    type RwTx<'db> = sanakirja::MutTxn<&'_ sanakirja::Env, ()>;
+
+    fn get(&self, ro_tx: &Self::RoTx<'_>, key: &T::Key) -> Result<Option<T::Value>, RuntimeError> {
         todo!()
     }
 
     fn get_range(
         &self,
+        ro_tx: &Self::RoTx<'_>,
         key: &T::Key,
         amount: usize,
     ) -> Result<impl Iterator<Item = T::Value>, RuntimeError> {
@@ -18,15 +22,20 @@ impl<T: Table> Database<T> for sanakirja::btree::Db<T::Key, T::Value> {
         Ok(iter)
     }
 
-    fn put(&mut self, key: &T::Key, value: &T::Value) -> Result<(), RuntimeError> {
+    fn put(
+        &mut self,
+        rx_tx: &mut Self::RwTx<'_>,
+        key: &T::Key,
+        value: &T::Value,
+    ) -> Result<(), RuntimeError> {
         todo!()
     }
 
-    fn clear(&mut self) -> Result<(), RuntimeError> {
+    fn clear(&mut self, rx_tx: &mut Self::RwTx<'_>) -> Result<(), RuntimeError> {
         todo!()
     }
 
-    fn delete(&mut self, key: &T::Key) -> Result<bool, RuntimeError> {
+    fn delete(&mut self, rx_tx: &mut Self::RwTx<'_>, key: &T::Key) -> Result<bool, RuntimeError> {
         todo!()
     }
 }
