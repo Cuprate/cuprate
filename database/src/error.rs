3a9633da2530d63d9eedce64cf1e003a8e5d8da7
hinto.janai <hinto.janai@protonmail.com>
2024-02-14 21:19:38 -0500

    error: add to error types

--- database/src/error.rs
+++ database/src/error.rs
@@ -7,21 +7,18 @@ use std::{borrow::Cow, fmt::Debug};
 //---------------------------------------------------------------------------------------------------- InitError
 /// Database errors that occur during initialization.
 ///
-/// `BackendError` is an error specifically from the
-/// database backend being used. TODO: this may not
-/// be needed if we can convert all error types into
-/// "generic" database errors.
+/// Many of these are the more common [`std::io::ErrorKind`]
+/// errors we'll run into, with better error messages.
 #[derive(thiserror::Error, Debug)]
 pub enum InitError {
-    /// TODO
-    #[error("database PATH is inaccessible: {0}")]
-    Path(std::io::Error),
+    /// I/O error.
+    #[error("database I/O error: {0}")]
+    Io(#[from] std::io::Error),
 
-    /// TODO
-    ///
-    /// An unknown error occurred.
-    #[error("unknown error: {0}")]
-    Unknown(Cow<'static, str>),
+    /// The given [`Path`]/[`File`]` existed and was accessible,
+    /// but was not a valid database file.
+    #[error("database file exists but is not valid")]
+    Invalid,
 }
 
 //---------------------------------------------------------------------------------------------------- RuntimeError
@@ -58,6 +55,15 @@ pub enum RuntimeError {
     /// space for more key/values.
     #[error("not enough space in database page")]
     PageFull,
+
+    /// Unknown error, the transaction should abort.
+    ///
+    /// TODO: this is for
+    /// <https://docs.rs/heed/latest/heed/enum.MdbError.html#variant.BadTxn>
+    ///
+    /// Can we even recover here? Should we panic?
+    #[error("transaction error, must abort")]
+    TxMustAbort,
 
     /// A [`std::io::Error`].
     #[error("I/O error: {0}")]
