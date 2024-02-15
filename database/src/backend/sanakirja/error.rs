65c5a1d40f04bdb1411f549f64ca7ed1ca4560d1
hinto.janai <hinto.janai@protonmail.com>
2024-02-15 15:14:04 -0500

    sanakirja: impl `From<sanakirja::Error>` for `InitError`

--- database/src/backend/sanakirja/error.rs
+++ database/src/backend/sanakirja/error.rs
@@ -2,7 +2,30 @@
 
 //---------------------------------------------------------------------------------------------------- Import
 
-//---------------------------------------------------------------------------------------------------- Error
+//---------------------------------------------------------------------------------------------------- InitError
+#[allow(clippy::fallible_impl_from)] // We need to panic sometimes
+impl From<sanakirja::Error> for crate::InitError {
+    fn from(error: sanakirja::Error) -> Self {
+        use sanakirja::Error as E;
+
+        match error {
+            E::IO(io_error) => Self::Io(io_error),
+            E::VersionMismatch => Self::InvalidVersion,
+
+            // A CRC failure essentially  means a `sanakirja` page was corrupt.
+            // <https://docs.rs/sanakirja/latest/sanakirja/enum.Error.html#variant.CRC>
+            E::Corrupt(_) | E::CRC(_) => Self::Corrupt,
+
+            // A database lock was poisoned.
+            // If 1 thread panics, everything should panic, so panic here.
+            //
+            // <https://docs.rs/sanakirja/latest/sanakirja/enum.Error.html#variant.Poison>
+            E::Poison => panic!("sanakirja database lock poison"),
+        }
+    }
+}
+
+//---------------------------------------------------------------------------------------------------- RuntimeError
 #[allow(clippy::fallible_impl_from)] // We need to panic sometimes
 impl From<sanakirja::Error> for crate::RuntimeError {
     fn from(error: sanakirja::Error) -> Self {
@@ -10,7 +33,7 @@ impl From<sanakirja::Error> for crate::RuntimeError {
 
         match error {
             E::IO(io_error) => Self::Io(io_error),
-            E::VersionMismatch => Self::VersionMismatch,
+            E::VersionMismatch => Self::InvalidVersion,
 
             // A CRC failure essentially  means a `sanakirja` page was corrupt.
             // <https://docs.rs/sanakirja/latest/sanakirja/enum.Error.html#variant.CRC>
