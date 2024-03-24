98ee5e3dd9c1700e0771e25bb00f0b3e2fdade1a
hinto.janai <hinto.janai@protonmail.com>
2024-03-24 16:15:23 -0400

    extended_block_header: impl `Pod`, fix layout

--- types/src/extended_block_header.rs
+++ types/src/extended_block_header.rs
@@ -6,24 +6,32 @@ use bytemuck::{AnyBitPattern, NoUninit, Pod, Zeroable};
 #[cfg(feature = "serde")]
 use serde::{Deserialize, Serialize};
 
-use crate::hard_fork::HardFork;
-
 //---------------------------------------------------------------------------------------------------- ExtendedBlockHeader
 /// TODO
-#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Pod, Zeroable)]
+#[repr(C)]
 pub struct ExtendedBlockHeader {
     /// TODO
-    pub version: HardFork,
+    pub version: u32, // TODO: conversion into HardFork?
     /// TODO
-    pub vote: HardFork,
+    pub vote: u32, // TODO: conversion into HardFork?
+
+    // TODO: the above 2 fields are `u32` instead of
+    // `u8` due to padding reasons. We could also add
+    // a `[u8; 6]` here in the middle.
     /// TODO
     pub timestamp: u64,
     /// TODO
     pub cumulative_difficulty: u128,
+
+    // TODO: The below used to be `usize` but
+    // we must ensure stable layout so they
+    // are set to `u64` (same since cuprate is
+    // only planned for 64-bit targets?)
     /// TODO
-    pub block_weight: usize,
+    pub block_weight: u64,
     /// TODO
-    pub long_term_weight: usize,
+    pub long_term_weight: u64,
 }
 
 //---------------------------------------------------------------------------------------------------- Tests
