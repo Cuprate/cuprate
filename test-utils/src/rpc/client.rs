d6e8fd3473d84e622fa2021c59a7f7e82f4fbcdc
hinto.janai <hinto.janai@protonmail.com>
2024-04-18 17:48:53 -0400

    client: use `spawn_blocking`

--- test-utils/src/rpc/client.rs
+++ test-utils/src/rpc/client.rs
@@ -1,32 +1,39 @@
 //! TODO
 
+//---------------------------------------------------------------------------------------------------- Use
 use std::sync::Arc;
 
 use serde::Deserialize;
-//---------------------------------------------------------------------------------------------------- Use
 use serde_json::json;
+use tokio::task::spawn_blocking;
 
 use monero_serai::{
     block::Block,
-    rpc::{HttpRpc, Rpc, RpcError},
-    transaction::Transaction,
+    rpc::{HttpRpc, Rpc},
 };
 
-use cuprate_types::{ExtendedBlockHeader, TransactionVerificationData, VerifiedBlockInformation};
+use cuprate_types::{TransactionVerificationData, VerifiedBlockInformation};
 
 //---------------------------------------------------------------------------------------------------- TODO
 /// TODO
-///
-/// TODO: Assumes non-restricted RPC.
 pub struct HttpRpcClient {
     address: String,
     rpc: Rpc<HttpRpc>,
 }
 
 impl HttpRpcClient {
-    /// TODO
+    /// Create an [`HttpRpcClient`].
+    ///
+    /// `address` should be an HTTP URL pointing to a `monerod`.
+    ///
+    /// If `None` is provided the default is used: `http://127.0.0.1:18081`.
+    ///
+    /// Note that for [`Self::get_verified_block_information`] to work, the `monerod`
+    /// must be in unrestricted mode such that some fields (e.g. `pow_hash`) appear
+    /// in the JSON response.
     ///
     /// # Panics
+    /// This panics if the `address` is invalid or a connection could not be made.
     pub async fn new(address: Option<String>) -> Self {
         let address = address.unwrap_or_else(|| "http://127.0.0.1:18081".to_string());
 
@@ -36,20 +43,23 @@ impl HttpRpcClient {
         }
     }
 
-    /// TODO
+    /// The address used for this [`HttpRpcClient`].
+    #[allow(dead_code)]
     const fn address(&self) -> &String {
         &self.address
     }
 
-    /// TODO
+    /// Access to the inner RPC client for other usage.
+    #[allow(dead_code)]
     const fn rpc(&self) -> &Rpc<HttpRpc> {
         &self.rpc
     }
 
-    /// TODO
+    /// Request data and map the response to a [`VerifiedBlockInformation`].
     ///
     /// # Panics
-    /// TODO
+    /// This function will panic at any error point, e.g.,
+    /// if the node cannot be connected to, if deserialization fails, etc.
     pub async fn get_verified_block_information(&self, height: u64) -> VerifiedBlockInformation {
         #[derive(Debug, Deserialize)]
         struct Result {
@@ -89,43 +99,55 @@ impl HttpRpcClient {
         	"untrusted node detected, `pow_hash` will not show on these nodes - use a trusted node!"
         );
 
-        let block = Block::read(&mut hex::decode(result.blob).unwrap().as_slice()).unwrap();
+        let (block_hash, block) = spawn_blocking(|| {
+            let block = Block::read(&mut hex::decode(result.blob).unwrap().as_slice()).unwrap();
+            (block.hash(), block)
+        })
+        .await
+        .unwrap();
 
         let txs = self.rpc.get_transactions(&block.txs).await.unwrap();
-        let txs = txs
-            .into_iter()
-            .enumerate()
-            .map(|(i, tx)| {
-                let tx_hash = tx.hash();
-                assert_eq!(tx_hash, block.txs[i]);
-                TransactionVerificationData {
-                    tx_blob: tx.serialize(),
-                    tx_weight: tx.weight(),
-                    tx_hash,
-                    fee: 0, // TODO: how to get this from RPC/calculate?
-                    tx,
-                }
-            })
-            .map(Arc::new)
-            .collect();
 
-        let block_header = result.block_header;
-        let block_hash = <[u8; 32]>::try_from(hex::decode(block_header.hash).unwrap()).unwrap();
-        let pow_hash = <[u8; 32]>::try_from(hex::decode(block_header.pow_hash).unwrap()).unwrap();
+        spawn_blocking(move || {
+            let txs = txs
+                .into_iter()
+                .enumerate()
+                .map(|(i, tx)| {
+                    let tx_hash = tx.hash();
+                    assert_eq!(tx_hash, block.txs[i]);
+                    TransactionVerificationData {
+                        tx_blob: tx.serialize(),
+                        tx_weight: tx.weight(),
+                        tx_hash,
+                        fee: 0, // TODO: how to get this from RPC/calculate?
+                        tx,
+                    }
+                })
+                .map(Arc::new)
+                .collect();
 
-        // Assert the block hash matches.
-        assert_eq!(block.hash(), block_hash);
+            let block_header = result.block_header;
+            let block_hash_2 =
+                <[u8; 32]>::try_from(hex::decode(&block_header.hash).unwrap()).unwrap();
+            let pow_hash =
+                <[u8; 32]>::try_from(hex::decode(&block_header.pow_hash).unwrap()).unwrap();
 
-        VerifiedBlockInformation {
-            block,
-            txs,
-            block_hash,
-            pow_hash,
-            height: block_header.height,
-            generated_coins: block_header.reward,
-            weight: block_header.block_weight,
-            long_term_weight: block_header.long_term_weight,
-            cumulative_difficulty: block_header.cumulative_difficulty,
-        }
+            // Assert the block hash matches.
+            assert_eq!(block_hash, block_hash_2);
+
+            VerifiedBlockInformation {
+                block,
+                txs,
+                block_hash,
+                pow_hash,
+                height: block_header.height,
+                generated_coins: block_header.reward,
+                weight: block_header.block_weight,
+                long_term_weight: block_header.long_term_weight,
+                cumulative_difficulty: block_header.cumulative_difficulty,
+            }
+        })
+        .await
+        .unwrap()
     }
 }
