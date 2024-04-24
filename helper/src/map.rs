3060a0c7dc72f29e72f968f8db7ba343b7000c6d
hinto.janai <hinto.janai@protonmail.com>
2024-04-24 14:54:54 -0400

    helper: docs

--- helper/src/map.rs
+++ helper/src/map.rs
@@ -6,8 +6,8 @@
 
 //---------------------------------------------------------------------------------------------------- Use
 
-//---------------------------------------------------------------------------------------------------- cumulative_difficulty
-/// Split a [`u128`] numerical value into 2 64-bit values.
+//---------------------------------------------------------------------------------------------------- `(u64, u64) <-> u128`
+/// Split a [`u128`] value into 2 64-bit values.
 ///
 /// The tuple returned is `(low, high)` where `low` is the least significant
 /// 64-bits of `number`, and `high` is the most significant.
@@ -18,15 +18,15 @@
 ///
 /// ```rust
 /// # use cuprate_helper::map::*;
-/// let number = u128::MAX - 1;
+/// let value = u128::MAX - 1;
 /// let low = u64::MAX - 1;
 /// let high = u64::MAX;
 ///
-/// assert_eq!(split_u128_into_low_high_bits(number), (low, high));
+/// assert_eq!(split_u128_into_low_high_bits(value), (low, high));
 /// ```
 #[inline]
-pub const fn split_u128_into_low_high_bits(number: u128) -> (u64, u64) {
-    let bits = number.to_le_bytes();
+pub const fn split_u128_into_low_high_bits(value: u128) -> (u64, u64) {
+    let bits = value.to_le_bytes();
 
     let low = u64::from_le_bytes([
         bits[0], bits[1], bits[2], bits[3], bits[4], bits[5], bits[6], bits[7],
@@ -38,7 +38,7 @@ pub const fn split_u128_into_low_high_bits(number: u128) -> (u64, u64) {
     (low, high)
 }
 
-/// Combine 2 64-bit values into a single [`u128`] numerical value.
+/// Combine 2 64-bit values into a single [`u128`] value.
 ///
 /// The inputs:
 /// - `low_bits` are the _least_ significant 64-bits of `cumulative_difficulty`
@@ -46,17 +46,15 @@ pub const fn split_u128_into_low_high_bits(number: u128) -> (u64, u64) {
 ///
 /// Note that `low_bits` & `high_bits` should be `u64` representation of _bits_, not numerical values.
 ///
-/// Although, the output of this function is a _numerical_ `u128` value.
-///
 /// See [`split_u128_into_low_high_bits`] for the inverse function.
 ///
 /// ```rust
 /// # use cuprate_helper::map::*;
-/// let number = u128::MAX - 1;
+/// let value = u128::MAX - 1;
 /// let low = u64::MAX - 1;
 /// let high = u64::MAX;
 ///
-/// assert_eq!(combine_low_high_bits_to_u128(low, high), number);
+/// assert_eq!(combine_low_high_bits_to_u128(low, high), value);
 /// ```
 #[inline]
 pub const fn combine_low_high_bits_to_u128(low_bits: u64, high_bits: u64) -> u128 {
