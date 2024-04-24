d5627cb256a9846cd7c6286417988c3d7ba0b95b
hinto.janai <hinto.janai@protonmail.com>
2024-04-24 14:31:23 -0400

    helper: rename functions
    splitting bits isn't necessarily `cumulative_difficulty` specific

--- helper/src/map.rs
+++ helper/src/map.rs
@@ -7,21 +7,23 @@
 //---------------------------------------------------------------------------------------------------- Use
 
 //---------------------------------------------------------------------------------------------------- cumulative_difficulty
-/// Split a 128-bit `cumulative_difficulty` value into low and high bits.
+/// Split a [`u128`] value into low and high bits.
 ///
 /// The tuple returned is `(low, high)` where `low` is the least significant
 /// 64-bits of `cumulative_difficulty`, and `high` is the most significant.
 ///
-/// See [`cumulative_difficulty_from_low_high_bits`] for the inverse function.
+/// Note that the output of this function are `u64` representation of _bits_, not a numerical value.
+///
+/// See [`combine_low_high_bits_to_u128`] for the inverse function.
 ///
 /// ```rust
 /// # use cuprate_helper::map::*;
-/// //                            cumulative_difficulty          low         high
+/// //                               regular u128 value          low         high
 /// //                                                v            v         v
-/// assert_eq!(cumulative_difficulty_to_low_high_bits(u128::MAX), (u64::MAX, u64::MAX));
+/// assert_eq!(split_u128_into_low_high_bits(u128::MAX), (u64::MAX, u64::MAX));
 /// ```
-pub const fn cumulative_difficulty_to_low_high_bits(cumulative_difficulty: u128) -> (u64, u64) {
-    let bits = cumulative_difficulty.to_le_bytes();
+pub const fn split_u128_into_low_high_bits(u: u128) -> (u64, u64) {
+    let bits = u.to_le_bytes();
 
     let low = u64::from_le_bytes([
         bits[0], bits[1], bits[2], bits[3], bits[4], bits[5], bits[6], bits[7],
@@ -33,21 +35,25 @@ pub const fn cumulative_difficulty_to_low_high_bits(cumulative_difficulty: u128)
     (low, high)
 }
 
-/// Combine 2 64-bit values into a single 128-bit `cumulative_difficulty`.
+/// Combine 2 64-bit values into a single [`u128`].
 ///
 /// The inputs:
 /// - `low_bits` are the _least_ significant 64-bits of `cumulative_difficulty`
 /// - `high_bits` are the _most_ significant 64-bits of `cumulative_difficulty`
 ///
-/// See [`cumulative_difficulty_to_low_high_bits`] for the inverse function.
+/// Note that `low_bits` & `high_bits` are `u64` representation of _bits_, not a numerical value.
+///
+/// Although, the output of this function is a _numerical_ `u128` value.
+///
+/// See [`split_u128_into_low_high_bits`] for the inverse function.
 ///
 /// ```rust
 /// # use cuprate_helper::map::*;
-/// //                                              low         high       cumulative_difficulty
-/// //                                                v         v          v
-/// assert_eq!(cumulative_difficulty_from_low_high_bits(u64::MAX, u64::MAX), u128::MAX);
+/// //                                     low         high       regular u128 value
+/// //                                       v         v          v
+/// assert_eq!(combine_low_high_bits_to_u128(u64::MAX, u64::MAX), u128::MAX);
 /// ```
-pub const fn cumulative_difficulty_from_low_high_bits(low_bits: u64, high_bits: u64) -> u128 {
+pub const fn combine_low_high_bits_to_u128(low_bits: u64, high_bits: u64) -> u128 {
     let low = low_bits.to_le_bytes();
     let high = high_bits.to_le_bytes();
 
