82ad53851d600e5ea561abce60a29509d7548b86
hinto-janai <hinto.janai@protonmail.com>
2024-05-01 13:50:34 -0400

    Update database/src/unsafe_sendable.rs
    Co-authored-by: Boog900 <boog900@tutanota.com>

--- database/src/unsafe_sendable.rs
+++ database/src/unsafe_sendable.rs
@@ -12,7 +12,6 @@ use crate::storable::StorableVec;
 
 //---------------------------------------------------------------------------------------------------- Aliases
 #[derive(Copy, Clone, Debug, PartialEq, PartialOrd, Eq, Ord, Hash, TransparentWrapper)]
-#[allow(clippy::unsafe_derive_deserialize)] // see safety message on the Send impl
 #[repr(transparent)]
 /// A wrapper type that `unsafe`ly implements `Send` for any `T`.
 ///
