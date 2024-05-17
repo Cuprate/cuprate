c5fbbcc6e8c98e7ee4f647629d9972b3afc7ab66
Boog900 <boog900@tutanota.com>
2024-05-17 13:52:51 +0000

    P2P Client Pool (#121)
    * p2p changes
    
    * clippy
    
    * a few more docs
    
    * init cuprate-p2p
    
    * remove some unrelated code and add some docs
    
    * start documenting client_pool.rs
    
    * add more docs
    
    * typo
    
    * fix docs
    
    * use JoinSet in connection maintainer
    
    * small changes
    
    * Apply suggestions from code review
    
    Co-authored-by: hinto-janai <hinto.janai@protonmail.com>
    
    * review changes
    
    * Update p2p/cuprate-p2p/src/connection_maintainer.rs
    
    Co-authored-by: hinto-janai <hinto.janai@protonmail.com>
    
    ---------
    
    Co-authored-by: hinto-janai <hinto.janai@protonmail.com>

---
+++ p2p/cuprate-p2p/src/client_pool/disconnect_monitor.rs
@@ -0,0 +1,72 @@
+//! # Disconnect Monitor
+//!
+//! This module contains the [`disconnect_monitor`] task, which monitors connected peers for disconnection
+//! and then removes them from the [`ClientPool`] if they do.
+use std::{
+    future::Future,
+    pin::Pin,
+    sync::Arc,
+    task::{Context, Poll},
+};
+
+use futures::{stream::FuturesUnordered, StreamExt};
+use tokio::sync::mpsc;
+use tokio_util::sync::WaitForCancellationFutureOwned;
+use tracing::instrument;
+
+use monero_p2p::{client::InternalPeerID, handles::ConnectionHandle, NetworkZone};
+
+use super::ClientPool;
+
+/// The disconnect monitor task.
+#[instrument(level = "info", skip_all)]
+pub async fn disconnect_monitor<N: NetworkZone>(
+    mut new_connection_rx: mpsc::UnboundedReceiver<(ConnectionHandle, InternalPeerID<N::Addr>)>,
+    client_pool: Arc<ClientPool<N>>,
+) {
+    tracing::info!("Starting peer disconnect monitor.");
+
+    let mut futs: FuturesUnordered<PeerDisconnectFut<N>> = FuturesUnordered::new();
+
+    loop {
+        tokio::select! {
+            Some((con_handle, peer_id)) = new_connection_rx.recv() => {
+                tracing::debug!("Monitoring {peer_id} for disconnect");
+                futs.push(PeerDisconnectFut {
+                    closed_fut: con_handle.closed(),
+                    peer_id: Some(peer_id),
+                });
+            }
+            Some(peer_id) = futs.next() => {
+                tracing::debug!("{peer_id} has disconnected, removing from client pool.");
+                client_pool.remove_client(&peer_id);
+            }
+            else => {
+                tracing::info!("Peer disconnect monitor shutting down.");
+                return;
+            }
+        }
+    }
+}
+
+/// A [`Future`] that resolves when a peer disconnects.
+#[pin_project::pin_project]
+struct PeerDisconnectFut<N: NetworkZone> {
+    /// The inner [`Future`] that resolves when a peer disconnects.
+    #[pin]
+    closed_fut: WaitForCancellationFutureOwned,
+    /// The peers ID.
+    peer_id: Option<InternalPeerID<N::Addr>>,
+}
+
+impl<N: NetworkZone> Future for PeerDisconnectFut<N> {
+    type Output = InternalPeerID<N::Addr>;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let this = self.project();
+
+        this.closed_fut
+            .poll(cx)
+            .map(|_| this.peer_id.take().unwrap())
+    }
+}
---
+++ p2p/cuprate-p2p/src/lib.rs
@@ -0,0 +1,15 @@
+//! Cuprate's P2P Crate.
+//!
+//! This crate contains a [`ClientPool`](client_pool::ClientPool) which holds connected peers on a single [`NetworkZone`](monero_p2p::NetworkZone).
+//!
+//! This crate also contains the different routing methods that control how messages should be sent, i.e. broadcast to all,
+//! or send to a single peer.
+//!
+#![allow(dead_code)]
+
+pub mod client_pool;
+pub mod config;
+pub mod connection_maintainer;
+mod constants;
+
+pub use config::P2PConfig;
