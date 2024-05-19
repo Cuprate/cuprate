f28321a7ec86da4a5c4c328634bb581a4a4b4b58
Boog900 <54e72d8a-345f-4599-bd90-c6b9bc7d0ec5@aleeas.com>
2024-05-19 18:50:10 +0100

    remove crate doc for now

--- p2p/cuprate-p2p/src/lib.rs
+++ p2p/cuprate-p2p/src/lib.rs
@@ -1,10 +1,5 @@
 //! Cuprate's P2P Crate.
 //!
-//! This crate contains a [`ClientPool`](client_pool::ClientPool) which holds connected peers on a single [`NetworkZone`](monero_p2p::NetworkZone).
-//!
-//! This crate also contains the different routing methods that control how messages should be sent, i.e. broadcast to all,
-//! or send to a single peer.
-//!
 #![allow(dead_code)]
 
 use std::sync::Arc;
