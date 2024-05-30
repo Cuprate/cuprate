317b6a79dd088474cf85d29666f08e9de1bd9f24
Boog900 <54e72d8a-345f-4599-bd90-c6b9bc7d0ec5@aleeas.com>
2024-05-30 01:09:41 +0100

    init cuprated

---
+++ binaries/cuprated/src/main.rs
@@ -0,0 +1,7 @@
+mod network;
+mod p2p_request_handler;
+mod syncer;
+
+fn main() {
+    println!("Hello, world!");
+}
---
+++ binaries/cuprated/src/syncer.rs
@@ -0,0 +1,4 @@
+//! # The Syncer
+//!
+//! The syncer is the part of Cuprate that handles keeping the blockchain state, it handles syncing if
+//! we have fallen behind, and it handles incoming blocks.
