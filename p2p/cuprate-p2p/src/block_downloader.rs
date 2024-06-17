9f6a85e23f426640c5df4445e874b9e5b7eadcae
Boog900 <54e72d8a-345f-4599-bd90-c6b9bc7d0ec5@aleeas.com>
2024-06-18 00:56:41 +0100

    make `BlockDownloader` pub

--- p2p/cuprate-p2p/src/block_downloader.rs
+++ p2p/cuprate-p2p/src/block_downloader.rs
@@ -230,7 +230,7 @@ impl Ord for ReadyQueueBatch {
 /// - request the next chain entry
 /// - download an already requested batch of blocks (this might happen due to an error in the previous request
 /// or because the queue of ready blocks is too large, so we need the oldest block to clear it).
-struct BlockDownloader<N: NetworkZone, S, C> {
+pub struct BlockDownloader<N: NetworkZone, S, C> {
     /// The client pool.
     client_pool: Arc<ClientPool<N>>,
 
