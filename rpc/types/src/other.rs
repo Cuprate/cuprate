e0bcf5a850c49729f3cb17f5677ee115bb7b1a82
hinto-janai <hinto.janai@protonmail.com>
2024-07-10 16:39:56 -0400

    Update rpc/types/src/other.rs
    Co-authored-by: Boog900 <boog900@tutanota.com>

--- rpc/types/src/other.rs
+++ rpc/types/src/other.rs
@@ -54,7 +54,7 @@ define_request_and_response! {
 define_request_and_response! {
     get_alt_blocks_hashes,
     cc73fe71162d564ffda8e549b79a350bca53c454 =>
-    core_rpc_server_commands_defs.h => 370..=451,
+    core_rpc_server_commands_defs.h => 288..=308,
     GetAltBlocksHashes,
     Request {},
     AccessResponseBase {
