1e113f5f70c3bd65a569e81c5f03a6ab746f90b0
Boog900 <54e72d8a-345f-4599-bd90-c6b9bc7d0ec5@aleeas.com>
2024-07-28 21:33:39 +0100

    add some comments

--- storage/txpool/src/types.rs
+++ storage/txpool/src/types.rs
@@ -3,22 +3,31 @@ use bytemuck::{Pod, Zeroable};
 use cuprate_types::CachedVerificationState;
 use monero_serai::transaction::Timelock;
 
+/// [`CachedVerificationState`] in a format that can be stored into the database.
+///
+/// This type impls [`Into`] & [`From`] [`CachedVerificationState`].
 #[derive(Copy, Clone, Debug, PartialEq, PartialOrd, Eq, Ord, Hash, Pod, Zeroable)]
 #[repr(C)]
 pub struct RawCachedVerificationState {
+    /// The raw hash, will be all `0`s if there is no block hash that this is valid for.
     raw_valid_at_hash: [u8; 32],
+    /// The raw hard-fork, will be `0` if there is no hf this was validated at.
     raw_hf: u8,
+    /// The raw timestamp, will be `0` if there is no timestamp that needs to be passed for this to
+    /// be valid.
     raw_valid_past_timestamp: [u8; 8],
 }
 
 impl From<RawCachedVerificationState> for CachedVerificationState {
     fn from(value: RawCachedVerificationState) -> Self {
+        // if the hash is all `0`s then there is no hash this is valid at.
         if value.raw_valid_at_hash == [0; 32] {
             return CachedVerificationState::NotVerified;
         }
 
         let raw_valid_past_timestamp = u64::from_le_bytes(value.raw_valid_past_timestamp);
 
+        // if the timestamp is 0, there is no timestamp that needs to be passed.
         if raw_valid_past_timestamp == 0 {
             return CachedVerificationState::ValidAtHashAndHF {
                 block_hash: value.raw_valid_at_hash,
