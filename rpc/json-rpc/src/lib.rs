76f479ea68071aba3540fb727b2648064c3c16e1
hinto.janai <hinto.janai@protonmail.com>
2024-08-20 15:42:35 -0400

    cfgs `1 tab` -> `4 spaces`

--- rpc/json-rpc/src/lib.rs
+++ rpc/json-rpc/src/lib.rs
@@ -3,38 +3,38 @@
 // Forbid lints.
 // Our code, and code generated (e.g macros) cannot overrule these.
 #![forbid(
-	// `unsafe` is allowed but it _must_ be
-	// commented with `SAFETY: reason`.
-	clippy::undocumented_unsafe_blocks,
+    // `unsafe` is allowed but it _must_ be
+    // commented with `SAFETY: reason`.
+    clippy::undocumented_unsafe_blocks,
 
-	// Never.
-	unused_unsafe,
-	redundant_semicolons,
-	unused_allocation,
-	coherence_leak_check,
-	while_true,
+    // Never.
+    unused_unsafe,
+    redundant_semicolons,
+    unused_allocation,
+    coherence_leak_check,
+    while_true,
 
-	// Maybe can be put into `#[deny]`.
-	unconditional_recursion,
-	for_loops_over_fallibles,
-	unused_braces,
-	unused_labels,
-	keyword_idents,
-	non_ascii_idents,
-	variant_size_differences,
+    // Maybe can be put into `#[deny]`.
+    unconditional_recursion,
+    for_loops_over_fallibles,
+    unused_braces,
+    unused_labels,
+    keyword_idents,
+    non_ascii_idents,
+    variant_size_differences,
     single_use_lifetimes,
 
-	// Probably can be put into `#[deny]`.
-	future_incompatible,
-	let_underscore,
-	break_with_label_and_loop,
-	duplicate_macro_attributes,
-	exported_private_dependencies,
-	large_assignments,
-	overlapping_range_endpoints,
-	semicolon_in_expressions_from_macros,
-	noop_method_call,
-	unreachable_pub,
+    // Probably can be put into `#[deny]`.
+    future_incompatible,
+    let_underscore,
+    break_with_label_and_loop,
+    duplicate_macro_attributes,
+    exported_private_dependencies,
+    large_assignments,
+    overlapping_range_endpoints,
+    semicolon_in_expressions_from_macros,
+    noop_method_call,
+    unreachable_pub,
 )]
 // Deny lints.
 // Some of these are `#[allow]`'ed on a per-case basis.
@@ -56,26 +56,26 @@
     nonstandard_style
 )]
 #![allow(
-	// FIXME: this lint affects crates outside of
-	// `database/` for some reason, allow for now.
-	clippy::cargo_common_metadata,
+    // FIXME: this lint affects crates outside of
+    // `database/` for some reason, allow for now.
+    clippy::cargo_common_metadata,
 
-	// FIXME: adding `#[must_use]` onto everything
-	// might just be more annoying than useful...
-	// although it is sometimes nice.
-	clippy::must_use_candidate,
+    // FIXME: adding `#[must_use]` onto everything
+    // might just be more annoying than useful...
+    // although it is sometimes nice.
+    clippy::must_use_candidate,
 
-	// FIXME: good lint but too many false positives
-	// with our `Env` + `RwLock` setup.
-	clippy::significant_drop_tightening,
+    // FIXME: good lint but too many false positives
+    // with our `Env` + `RwLock` setup.
+    clippy::significant_drop_tightening,
 
-	// FIXME: good lint but is less clear in most cases.
-	clippy::items_after_statements,
+    // FIXME: good lint but is less clear in most cases.
+    clippy::items_after_statements,
 
-	clippy::module_name_repetitions,
-	clippy::module_inception,
-	clippy::redundant_pub_crate,
-	clippy::option_if_let_else,
+    clippy::module_name_repetitions,
+    clippy::module_inception,
+    clippy::redundant_pub_crate,
+    clippy::option_if_let_else,
 )]
 // Allow some lints in tests.
 #![cfg_attr(
