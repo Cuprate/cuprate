cfbd28796a7e468184400d12935ac5eb4e4c40b7
hinto.janai <hinto.janai@protonmail.com>
2024-09-02 12:54:04 -0400

    blockchain: `config/` -> `config.rs`

--- storage/blockchain/src/config.rs
+++ storage/blockchain/src/config.rs
@@ -1,4 +1,44 @@
-//! The main [`Config`] struct, holding all configurable values.
+//! Database configuration.
+//!
+//! This module contains the main [`Config`]uration struct
+//! for the database [`Env`](cuprate_database::Env)ironment,
+//! and blockchain-specific configuration.
+//!
+//! It also contains types related to configuration settings.
+//!
+//! The main constructor is the [`ConfigBuilder`].
+//!
+//! These configurations are processed at runtime, meaning
+//! the `Env` can/will dynamically adjust its behavior based
+//! on these values.
+//!
+//! # Example
+//! ```rust
+//! use cuprate_blockchain::{
+//!     cuprate_database::{Env, config::SyncMode},
+//!     config::{ConfigBuilder, ReaderThreads},
+//! };
+//!
+//! # fn main() -> Result<(), Box<dyn std::error::Error>> {
+//! let tmp_dir = tempfile::tempdir()?;
+//! let db_dir = tmp_dir.path().to_owned();
+//!
+//! let config = ConfigBuilder::new()
+//!      // Use a custom database directory.
+//!     .db_directory(db_dir.into())
+//!     // Use as many reader threads as possible (when using `service`).
+//!     .reader_threads(ReaderThreads::OnePerThread)
+//!     // Use the fastest sync mode.
+//!     .sync_mode(SyncMode::Fast)
+//!     // Build into `Config`
+//!     .build();
+//!
+//! // Start a database `service` using this configuration.
+//! let (_, _, env) = cuprate_blockchain::service::init(config.clone())?;
+//! // It's using the config we provided.
+//! assert_eq!(env.config(), &config.db_config);
+//! # Ok(()) }
+//! ```
 
 //---------------------------------------------------------------------------------------------------- Import
 use std::{borrow::Cow, path::Path};
