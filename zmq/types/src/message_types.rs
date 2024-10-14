40a26bca3c427e79ba1d243acb41d052b1163304
Dmitry Holodov <dimalinux@protonmail.com>
2024-10-14 15:44:22 -0700

    added FullChainMain

--- zmq/types/src/message_types.rs
+++ zmq/types/src/message_types.rs
@@ -6,11 +6,11 @@ use crate::bytes::Bytes;
 pub struct TxPoolAdd {
     pub version: u8,
     pub unlock_time: u64,
-    pub inputs: Vec<Input>,
+    pub inputs: Vec<PoolInput>,
     pub outputs: Vec<Output>,
     pub extra: Bytes<44>,
     signatures: Vec<NotUsed>,
-    pub ringct: RingCt,
+    pub ringct: PoolRingCt,
 }
 
 #[derive(Debug, Default, Clone, Serialize, Deserialize)]
@@ -21,9 +21,30 @@ pub struct TxPoolAddMin {
     pub fee: u64,
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+pub struct FullChainMain {
+    pub major_version: u8,
+    pub minor_version: u8,
+    pub timestamp: u64,
+    pub prev_id: Bytes<32>,
+    pub nonce: u32,
+    pub miner_tx: MinerTx,
+    pub tx_hashes: Vec<Bytes<32>>,
+}
+
 #[derive(Debug, Default, Clone, Serialize, Deserialize)]
-pub struct Input {
+pub struct PoolInput {
     pub to_key: ToKey,
+}
+
+#[derive(Debug, Default, Clone, Serialize, Deserialize)]
+pub struct MinerInput {
+    pub r#gen: Gen,
+}
+
+#[derive(Debug, Default, Clone, Serialize, Deserialize)]
+pub struct Gen {
+    pub height: u64,
 }
 
 #[derive(Debug, Default, Clone, Serialize, Deserialize)]
@@ -46,13 +67,17 @@ pub struct ToTaggedKey {
 }
 
 #[derive(Debug, Default, Clone, Serialize, Deserialize)]
-pub struct RingCt {
-    #[serde(rename = "type")]
-    pub ringct_type: u8,
+pub struct PoolRingCt {
+    pub r#type: u8,
     pub encrypted: Vec<Encrypted>,
     pub commitments: Vec<Bytes<32>>,
     pub fee: u64,
     pub prunable: Prunable,
+}
+
+#[derive(Debug, Default, Clone, Serialize, Deserialize)]
+pub struct MinerRingCt {
+    pub r#type: u8,
 }
 
 #[derive(Debug, Default, Clone, Copy, Serialize, Deserialize)]
@@ -94,6 +119,17 @@ pub struct Clsag {
     pub s: Vec<Bytes<32>>,
     pub c1: Bytes<32>,
     pub D: Bytes<32>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct MinerTx {
+    pub version: u8,
+    pub unlock_time: u64,
+    pub inputs: Vec<MinerInput>,
+    pub outputs: Vec<Output>,
+    pub extra: Bytes<52>,
+    signatures: Vec<NotUsed>,
+    pub ringct: MinerRingCt,
 }
 
 #[cfg(test)]
@@ -256,6 +292,133 @@ mod tests {
 
         let tx_pool_adds: Vec<TxPoolAddMin> = serde_json::from_value(json1.clone()).unwrap();
         let json2 = serde_json::to_value(&tx_pool_adds).unwrap();
+        assert_json_eq!(json1, json2);
+    }
+
+    #[test]
+    fn test_full_chain_main_json() {
+        let json1 = json!([
+          {
+            "major_version": 16,
+            "minor_version": 16,
+            "timestamp": 1726973843,
+            "prev_id": "ce3731311b7e4c1e58a2fe902dbb5c60bb2c0decc163d5397fa52a260d7f09c1",
+            "nonce": 537273946,
+            "miner_tx": {
+              "version": 2,
+              "unlock_time": 3242818,
+              "inputs": [
+                {
+                  "gen": {
+                    "height": 3242758
+                  }
+                }
+              ],
+              "outputs": [
+                {
+                  "amount": 618188180000_u64,
+                  "to_tagged_key": {
+                    "key": "83faf44df7e9fb4cf54a8dd6a63868507d1a1896bdb35ea9110d739d5da6cf21",
+                    "view_tag": "38"
+                  }
+                }
+              ],
+              "extra": "010e3356a86dbb339354afbc693408dfe8648bffd0b276e6a431861eb73643d88d02115162e362c98e2d00000000000000000000",
+              "signatures": [],
+              "ringct": {
+                "type": 0
+              }
+            },
+            "tx_hashes": [
+              "2c1b67d3f10b21270cac116e6d5278dc4024ee2d727e4ad56d6dedb1abc0270c",
+              "c2cfec0de23229a2ab80ca464cef66fc1cad53647a444f048834ec236c38c867",
+              "03c7649af2373c0f739d3c2eff9ee1580986b460d2abdd5e2aa332281e52da7e",
+              "1e0834cc658599e786040bdcd9b589a5e8d975233b72279d04ece1a3dd5572b0",
+              "ba65c30150e906a8799ee99bb2e6481873e42ed8b025cf967c5798528ddc81b4",
+              "6fc7b1da1cf433edafb142173e9ac13fe05142a36d8a72e9efdf7a3b94da11d6",
+              "847c06dcda4540d45cae868d4d031781bd87d9bfa4b2186a611428f52e68ccee",
+              "79f87a1b2fc17295d2cf25b6a65dd17fd8630829ee50f9c48f15e4a24e72d872",
+              "32b4f7ce6d864006b274dbd73fc8058151d0fd2dd0bb4b423120e32451fd59eb",
+              "430fe7fa00b63b68b301a4e4810bef2b5be1f651dba8c360e86eac61227382e7",
+              "9f8d2bf5e39071abccb336404ea72ab85cb731500a1d386a3bf537b9046df29d",
+              "f63893b8226ca28c290cb65541d60c1675dda1e2a77a629b6b2d7c3886240b23",
+              "ee8608b6e80cce14beaf95f747f4da8e40e40a49ad1adc20038843a6da3df3c6",
+              "05783765c150ed1e46d6380726e7ca1f788305754e553f5f2d49b9f09aaaf88d",
+              "20b4b95e62f45b72014d6ab14edb0b31e273cdc8c8d106068dd32ef6e92fc0a2",
+              "9230fb0a9dce8e2ca7e109ebf3480838251691de8ed73ea91f74723c5cf19bac",
+              "d59cf84a25f56ec0f1352bb05645efe9b9326598c4f7c5bc39a87eb7a20c48fc",
+              "465deb73c48a460df71861d61666dabb906648035a1fecfd0e988ee37616c655",
+              "5767bc633729ba4555561510f3db739431b16744234dcd549a0d346eaa6685b1",
+              "2c8d9af5d5774de96e67835ac5adbc6ca5579125b08bc907b395645eea6410ec",
+              "d385c884a0687c3360725dd3a3f6acf6f64bf38d8eeea1644d80bc23b13ee870",
+              "b2bc7e9fa9c1da08a8b6ee58505611c05bc388fd30aece00e9a0041470f7e950",
+              "69a4a79b50d42d372e91c6608c2652d1d5ddd343526c387ef6cf1e3c158b1765",
+              "ef508dfa79bbedd226835c42a9d000a64cc4abe0250c9aa55fd968224e2b45c3",
+              "0413c3b3fc621c472e10a102d77456db506f0df10a909833aed0c6738fb31eeb",
+              "e0c52d6d649c2f1abce4c6ffce4dd75a23308afbb6abe33af53da232c40caf5f",
+              "cd1fd68d2a15002ca6236083ff6ae165c8fd922f410da79640a4342fd8ebd1c8",
+              "ba746f80ca4ee496f4188ba278f1ed69a913238450d52bd2e2f3d3bf6fdd43d3",
+              "13c964bc13a55621b7bbbfe9a6d703536d951bfa19eedee93dd1286020959021",
+              "41a6f8d0df227a401a9bd6f5c0fbc21ed89f515ea5c8434a087e8b880080ee1f",
+              "41c2b5994284790b1ee158f7b87aa1231c14975d6456a91ff6f93c6f81277965",
+              "7e6b7f169cc6cab88e652771157cf8c2eb6f69dffb6939a79b34c6554fe6c00b",
+              "619517d9d138bf95c6b77eb801526b8419616de2b8618ccfd3b6d1c10364bc64",
+              "52cca64fb20fc2f6d06034a1a2d9b5665972ebc2569ec69f8d473caada309add",
+              "219c106d09da5a27b339ea0f070da090779b31ef9ccfa90d6d25e7388341eff9",
+              "e07ce6e96e73cff80c9cc4c1b349ad1ef53cff210b876d4e7afd89fcc8b2e5dd",
+              "e98f2a00b2892cd65c0252d956d88a4bb8024c7db98ca003c127b097f097f276",
+              "ed81aa398071fe495e37095e51ff50053e132bd11f27ba9c06ac4bf4063b756f",
+              "667d29a0cefa311e06fcfc22c98ef75edf81deb6c8a812492eb255a049c826db",
+              "8b16e8cbc1765247456bd67a3106498f686401b7529dc0f6b03360caf8671135",
+              "013e443e63259748f6d1a5653374826618ba066b7febcf55c829333f0dd9a6c3",
+              "517a05d82de59a973eb4d343c45558841c9165ccd75ca7c9d2e1a35f80c26c15",
+              "af74d5dd44cfed8f40f853a6fc405dae23d547482296f8dbbc13c1aed2c3d8c5",
+              "b5086746e805d875cbbbbb49e19aac29d9b75019f656fab8516cdf64ac5cd346",
+              "cfcda18d058656797a1272b384774dcfc26a504a24298aa49ba060eb6b4a19e0",
+              "1f380660a99030cc45f85ba8ee0e0541035c0fde719c84aa692796328974c9dd",
+              "53127181a0301a27b3a2749dc997556b211d949a99aa34d1c52d5c54220f49d2",
+              "5d50a66df97f4decc4ecc3f5030589ef966d5af84a995f7fb14f1c02ae9704db",
+              "cdab9628acdb57c460e292660e7a07caf2ddbcffdfff92f3e5e4fb12119a11ca",
+              "e740a098a74d7a66a821c4ac3c5f913a82fc7445b5593cc5fa3e48ad1b4589b1",
+              "760549176fec210cfe0ff58eabbf2670cf33b4cd3942a3b60a98bf8f328a6d01",
+              "961b0956aa6303ed8ca1687d93ed46b9aa8a0203ec4ce0cbc2e86b364fbfb613",
+              "b9db041b2c3bfc6b5b0facb638b0b4643eec76b060039a6b11fb43682ed77a97",
+              "1011c321eb386b9975e8124bdb130790dcf4ac0021da3103cabbf7dfa18ccea7",
+              "6a9d3d15be4b25bd544d96bb1d7685e53f9484735bb22994feffb9037009aeeb",
+              "bf20d6193890cf7fdead9e3b60197564c663b5a62eda782a49d4aa7819bb9665",
+              "472d28f9d25a95e625eb808ff3827e7f6792009e1ba0b3b21951f3058b65a75d",
+              "e3931b2b66da07f983d2235d9d0b3a3098008458bdc0c1ad4370fae73e1eaa9e",
+              "e18a0dea6382c95aa4089a971190683b171e9405c06fd4111924144600f3bcf3",
+              "1a336bcf24026307821b76b9ca18b178c285c591c5df9906e3ffbd2050ccd356",
+              "8ca2d0e5ae9b9981bb8b76ba0da383c585664b2a2f4e861d58aab00c9b0cc808",
+              "e1866c27023ccea276034c4d572eab42713132e4fdb2aafa9488f6d74cd49303",
+              "3674cfafba4cdea5775a72a82e5d553bf180beab456b3cbaa7b41a1574fe1948",
+              "9bb400dd317425f40176c3094a5573037b0217e0b60761cb66a8fa15b63b36c3",
+              "c078048028aca3e9bc40f68f4d42ef25c6af2cef4da20bf3be70dd6a23b82d52",
+              "c28cc85f945085e70259ed02131ae3f8c5992e789c9c75c2c6e257306beaf26e",
+              "4c2b121795fe2b90fda84813543952382daa29c7b96edd9f96040df13e48e347",
+              "63c6fba30b5471fd60e715cbaf4448badafde68dbc42c54d96b56dd2c4bf2d15",
+              "a4240138ecfe736113581f318f261a01992eaa8fa5b7bd6938d9dbeb65aa85d7",
+              "b9d088a7b21f655d0cf50f8404e874f4d1655fb5565a354d2c0dd6d113619c66",
+              "9133e7e98a83f6e10a7fd44c104d9124d93e0d3e920f5c160873b394dd3a2fcb",
+              "953985dbd0ea6f86746e83be144ec2ff2897ef1f3506eede083b893e98dd63ea",
+              "83af840c4cad46de96c86fcf700ade32e73260d4a16cefa330cb5a722ef59fdf",
+              "eea3c0c2b016ea0c269f954fd8172c3d118f08103c9842b81b05290c9faf3780",
+              "ac43a363fdb81fa4f6df1cb06ba49a5f4eeef411957cf2afad55cbc1e79bc4d1",
+              "ca72cf7bda22aed15c16ca67e7b6cc57109cdc86d4ffe38fd71210a5380fcada",
+              "477dc1cd62106d9df6b37f8515579a48d01b310387087c08ce7062a8eb5df98d",
+              "d47b6dcd3b13288825c954df6c6e30eb683d1f79434beaee7172082f8ae74280",
+              "9c64ef20c69589c56fcc5f3a0d10f6957ecea248e44acb432aaf16a88eeef946",
+              "d2aa256bfd61bdb64ac38da6cbc3e77fb315bb9fbaf422087c10345377df44f6",
+              "8b9623e4513594a6eaeb3475ea7d0eb585dd8f6e20e21c316db0b942fada2336",
+              "860725ed0bd18c744e6b8b02888ad88be1cf23d7153131b220a0f9fbb76976bf",
+              "387cc6e807efc263a0ad6a30e6313a27d16abef038264d0afa0e6ad943be55da"
+            ]
+          }
+        ]);
+
+        let full_chain_main: Vec<FullChainMain> = serde_json::from_value(json1.clone()).unwrap();
+        let json2 = serde_json::to_value(&full_chain_main).unwrap();
         assert_json_eq!(json1, json2);
     }
 }
