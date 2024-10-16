978d72b6c14f27bd61836d90f304d766cc7a93f2
SyntheticBird <118022351+SyntheticBird45@users.noreply.github.com>
2024-10-16 23:17:58 +0000

    Move consensus context service into a subcrate. (#318)
    Co-authored-by: Boog900 <boog900@tutanota.com>

--- consensus/src/tests/context/difficulty.rs
+++ consensus/src/tests/context/difficulty.rs
@@ -4,10 +4,10 @@ use proptest::collection::{size_range, vec};
 use proptest::{prelude::*, prop_assert_eq, prop_compose, proptest};
 
 use crate::{
-    context::difficulty::*,
     tests::{context::data::DIF_3000000_3002000, mock_db::*},
     HardFork,
 };
+use cuprate_consensus_context::difficulty::*;
 use cuprate_helper::num::median;
 use cuprate_types::Chain;
 
