499ba938dfc85571b3a10b3b8dc90d4e782d15a1
Boog900 <boog900@tutanota.com>
2025-01-08 01:27:54 +0000

    Update rpc/types/src/bin.rs
    Co-authored-by: hinto-janai <hinto.janai@protonmail.com>

--- rpc/types/src/bin.rs
+++ rpc/types/src/bin.rs
@@ -133,6 +133,7 @@ define_request_and_response! {
         current_height: u64,
         output_indices: Vec<BlockOutputIndices>,
         daemon_time: u64 = default_zero::<u64>(), "default_zero",
+        // FIXME: use `default()` after <https://github.com/Cuprate/cuprate/pull/355>
         pool_info: PoolInfo = PoolInfo::None, "PoolInfo::default",
     }
 }
