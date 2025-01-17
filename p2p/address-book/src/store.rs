0f902eaca765b1ecab5f69ed745d8ec4c882e6ce
Boog900 <54e72d8a-345f-4599-bd90-c6b9bc7d0ec5@aleeas.com>
2025-01-17 18:41:02 +0000

    fix clippy

--- p2p/address-book/src/store.rs
+++ p2p/address-book/src/store.rs
@@ -40,7 +40,7 @@ pub(crate) fn save_peers_to_disk<Z: BorshNetworkZone>(
     .unwrap();
 
     let dir = cfg.peer_store_directory.clone();
-    let file = dir.join(format!("{}", Z::NAME));
+    let file = dir.join(Z::NAME);
 
     spawn_blocking(move || {
         fs::create_dir_all(dir)?;
@@ -57,9 +57,7 @@ pub(crate) async fn read_peers_from_disk<Z: BorshNetworkZone>(
     ),
     std::io::Error,
 > {
-    let file = cfg
-        .peer_store_directory
-        .join(format!("{}", Z::NAME));
+    let file = cfg.peer_store_directory.join(Z::NAME);
 
     tracing::info!("Loading peers from file: {} ", file.display());
 
