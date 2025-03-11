8de96efd0dd83d5da85d729912ac6a5c54d041d2
hinto.janai <hinto.janai@protonmail.com>
2025-03-11 16:33:16 -0400

    update

--- binaries/cuprated/src/killswitch.rs
+++ binaries/cuprated/src/killswitch.rs
@@ -32,8 +32,8 @@ const _: () = {
 
 /// The killswitch activates if the current timestamp is ahead of this timestamp.
 ///
-/// Wed Apr  9 12:00:00 AM UTC 2025
-pub const KILLSWITCH_ACTIVATION_TIMESTAMP: u64 = 1744156800;
+/// Wed Apr 16 12:00:00 AM UTC 2025
+pub const KILLSWITCH_ACTIVATION_TIMESTAMP: u64 = 1744761600;
 
 /// Check if the system clock is past a certain timestamp,
 /// if so, exit the entire program.
@@ -44,8 +44,8 @@ fn killswitch() {
     /// sanity checking the system's clock to make
     /// sure it is not overly behind.
     ///
-    /// Tue Mar 11 08:21:40 PM UTC 2025
-    const SYSTEM_CLOCK_SANITY_TIMESTAMP: u64 = 1741724500;
+    /// Tue Mar 11 08:33:20 PM UTC 2025
+    const SYSTEM_CLOCK_SANITY_TIMESTAMP: u64 = 1741725200;
 
     let current_ts = current_unix_timestamp();
 
