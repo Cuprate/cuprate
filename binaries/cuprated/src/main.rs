c3bb47cff9e4e95c87f68cb6edfe129371a4ce31
hinto.janai <hinto.janai@protonmail.com>
2025-03-21 17:07:18 -0400

    add exit msg

--- binaries/cuprated/src/main.rs
+++ binaries/cuprated/src/main.rs
@@ -149,6 +149,14 @@ fn main() {
             .await
             .unwrap();
     });
+
+    // TODO: graceful tokio shutdown.
+    // - First Ctrl+C = tell subsystems to shutdown and await
+    // - Second Ctrl+C = forceful shutdown
+    info!(
+        "Exiting cuprated. Total uptime: {}s",
+        statics::START_INSTANT.elapsed().unwrap().as_secs()
+    );
 }
 
 /// Initialize the [`tokio`] runtime.
