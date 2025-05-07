505479d1bf30bf06b13370b5fc7c6075db732318
Boog900 <54e72d8a-345f-4599-bd90-c6b9bc7d0ec5@aleeas.com>
2025-05-07 20:54:15 +0100

    fix epee bin OOM

---
+++ net/epee-encoding/tests/out_of_memory.rs
@@ -0,0 +1,32 @@
+#![expect(unused_crate_dependencies, reason = "outer test module")]
+
+use cuprate_epee_encoding::{epee_object, from_bytes};
+
+struct T {
+    a: Vec<String>,
+}
+
+epee_object!(
+    T,
+    a: Vec<String>,
+);
+
+#[test]
+fn out_of_memory() {
+    #[rustfmt::skip]
+    let mut data = [
+        // header
+        0x01, 0x11, 0x01, 0x1, 0x01, 0x01, 0x02, 0x1, 0x1,
+        // struct + field
+        0x04, 0x01, b'a',
+        // field tag
+        0x80 | 10,
+        // varint length of len
+        0x03,
+        // len, as big as possible
+        0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff
+    ]
+        .to_vec();
+
+    drop(from_bytes::<T, _>(&mut data.as_slice()));
+}
