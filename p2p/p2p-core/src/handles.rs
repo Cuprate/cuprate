6430de6fe4a6ba3eec953d00187869e0e56d1147
zyc <18611145971@163.com>
2025-05-30 21:32:40 +0800

    Fix

--- p2p/p2p-core/src/handles.rs
+++ p2p/p2p-core/src/handles.rs
@@ -34,10 +34,7 @@ impl HandleBuilder {
     /// This will panic if a permit was not set [`HandleBuilder::with_permit`]
     pub fn build(self) -> (ConnectionGuard, ConnectionHandle) {
         let token = CancellationToken::new();
-
-        if self.permit.is_none(){
-            panic!("Permit is not set.");
-        }
+        assert!(!self.permit.is_none(), "Permit is not set.");
         (
             ConnectionGuard {
                 token: token.clone(),
@@ -95,10 +92,10 @@ impl ConnectionHandle {
     /// Bans the peer for the given `duration`.
     pub fn ban_peer(&self, duration: Duration) {
         #[expect(
-            clippy::let_underscore_must_use,
-            reason = "error means peer is already banned; fine to ignore"
+        clippy::let_underscore_must_use,
+        reason = "error means peer is already banned; fine to ignore"
         )]
-        let _ = self.ban.set(BanPeer(duration));
+            let _ = self.ban.set(BanPeer(duration));
         self.token.cancel();
     }
     /// Checks if this connection is closed.
