ed58edb1a91dd49c4d81d42382218affbdd85e8c
Boog900 <54e72d8a-345f-4599-bd90-c6b9bc7d0ec5@aleeas.com>
2025-11-30 20:29:00 +0000

    commit monitor

---
+++ binaries/cuprated/src/monitor.rs
@@ -0,0 +1,39 @@
+use tokio::sync::watch;
+use tokio_util::sync::CancellationToken;
+use tokio_util::task::TaskTracker;
+
+pub fn new() -> (CupratedMonitor, CupratedTask) {
+    let (error_set, error_watch) = watch::channel(false);
+    let task_trackers = TaskTracker::new();
+    let cancellation_token = CancellationToken::new();
+
+    (
+        CupratedMonitor {
+            task_trackers: task_trackers.clone(),
+            error_watch,
+            cancellation_token: cancellation_token.clone(),
+        },
+        CupratedTask {
+            task_tracker: task_trackers,
+            error_set,
+            cancellation_token
+        }
+        )
+}
+
+pub struct CupratedMonitor {
+    pub task_trackers: TaskTracker,
+    pub error_watch: watch::Receiver<bool>,
+    pub cancellation_token: CancellationToken,
+}
+
+#[derive(Clone)]
+pub struct CupratedTask {
+    pub task_tracker: TaskTracker,
+    pub error_set: watch::Sender<bool>,
+    pub cancellation_token: CancellationToken,
+}
+
+
+
+
