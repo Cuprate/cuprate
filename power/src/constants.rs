1da93cd6512370f8e8e4c1c042e8a4d45eb1399c
hinto.janai <hinto.janai@protonmail.com>
2026-01-27 00:33:22 +0000

    update diff numbers

--- power/src/constants.rs
+++ power/src/constants.rs
@@ -4,28 +4,28 @@ pub const POWER_INPUT_THRESHOLD: usize = 8;
 /// Number of recent block hashes viable for RPC.
 pub const POWER_HEIGHT_WINDOW: usize = 2;
 
-/// Fixed difficulty for valid PoW.
+/// Fixed difficulty for the difficulty formula.
 ///
 /// Target time = ~1s of single-threaded computation.
-///
-/// The difficulty value and computation time
-/// are directly proportional, in other words:
-/// - `DIFFICULTY = 200` takes twice as long as
-/// - `DIFFICULTY = 100` takes twice as long as
-/// - `DIFFICULTY = 50` and so on
-///
-/// Reference values; value of machines are measured in
-/// seconds and rounded to the expected average given
-/// enough computation:
+/// The difficulty value and computation time have a quadratic relationship.
+/// Reference values; value of machines are measured in seconds:
 ///
 /// | Difficulty | Raspberry Pi 5 | Ryzen 5950x | Mac mini M4 |
 /// |------------|----------------|-------------|-------------|
-/// | 25         | 0.6            | 0.15        | 0.09        |
-/// | 50         | 1.2            | 0.30        | 0.18        |
-/// | 100        | 2.4            | 0.60        | 0.37        |
-/// | 200        | 4.8            | 1.20        | 0.75        |
-/// | 400        | 9.6            | 2.40        | 1.50        |
-pub const POWER_DIFFICULTY: u32 = 200;
+/// | 0          | 0.024          | 0.006       | 0.005       |
+/// | 25         | 0.307          | 0.076       | 0.067       |
+/// | 50         | 0.832          | 0.207       | 0.187       |
+/// | 75         | 1.654          | 0.395       | 0.373       |
+/// | 100        | 2.811          | 0.657       | 0.611       |
+/// | 125        | 4.135          | 0.995       | 0.918       |
+/// | 150        | 5.740          | 1.397       | 1.288       |
+/// | 175        | 7.740          | 1.868       | 1.682       |
+/// | 200        | 9.935          | 2.365       | 2.140       |
+/// | 225        | 12.279         | 2.892       | 2.645       |
+/// | 250        | 14.855         | 3.573       | 3.226       |
+/// | 275        | 17.736         | 4.378       | 3.768       |
+/// | 300        | 20.650         | 5.116       | 4.422       |
+pub const POWER_DIFFICULTY: u32 = 100;
 
 /// Max difficulty value.
 ///
