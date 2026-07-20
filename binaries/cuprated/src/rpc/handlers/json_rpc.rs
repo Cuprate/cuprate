26cb0c2b59e17a1b1ab546c0f574ed53aa81ea15
redsh4de <25299353+redsh4de@users.noreply.github.com>
2026-07-20 10:48:27 +0300

    reorder

--- binaries/cuprated/src/rpc/handlers/json_rpc.rs
+++ binaries/cuprated/src/rpc/handlers/json_rpc.rs
@@ -488,9 +488,6 @@ pub(super) async fn get_info(
         (String::new(), false)
     };
 
-    let target_height = state.syncer_handle.target_height();
-    let busy_syncing = target_height > 0;
-
     let (cumulative_difficulty, cumulative_difficulty_top64) =
         split_u128_into_low_high_bits(cumulative_difficulty);
 
@@ -545,6 +542,8 @@ pub(super) async fn get_info(
         state.start_instant_unix
     };
 
+    let target_height = state.syncer_handle.target_height();
+    let busy_syncing = target_height > 0;
     let synchronized = !busy_syncing;
     let target = c.current_hf.block_time().as_secs();
     let top_block_hash = Hex(c.top_hash);
